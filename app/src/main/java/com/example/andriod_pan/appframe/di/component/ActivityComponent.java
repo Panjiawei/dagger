24e021d9a25020afb29824b175546c06e25c19e8
潘嘉伟 <1042405630@qq.com>
2018-10-26 00:01:17 +0800

    修改

--- app/src/main/java/com/example/andriod_pan/appframe/di/component/ActivityComponent.java
+++ app/src/main/java/com/example/andriod_pan/appframe/di/component/ActivityComponent.java
@@ -1,10 +1,10 @@
-package com.example.andriod_pan.appframe.di.di.component;
+package com.example.andriod_pan.appframe.di.component;
 
 import android.app.Activity;
 
 
-import com.example.andriod_pan.appframe.di.di.module.ActivityModule;
-import com.example.andriod_pan.appframe.di.di.scope.ActivityScope;
+import com.example.andriod_pan.appframe.di.module.ActivityModule;
+import com.example.andriod_pan.appframe.di.scope.ActivityScope;
 
 import dagger.Component;
 
@@ -14,7 +14,7 @@ import dagger.Component;
  * @author wujiajun
  */
 @ActivityScope
-@Component(dependencies = AppComponent.class, modules = ActivityModule.class)
+@Component(dependencies = com.example.andriod_pan.appframe.di.di.component.AppComponent.class, modules = ActivityModule.class)
 public interface ActivityComponent {
 
     /**
